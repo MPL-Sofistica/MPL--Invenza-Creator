12671a0c9d23d1065b46a1a2fbbdd1b5db4c639d
IngsergioVTmpl <sergio.valdivia@mpl.com.co>
2020-09-03 14:53:06 -0500

    Probando Funcionalidad de Bolt
    Se Crea un Script Basico para la funcionalidad de Bolt

---
+++ Assets/Editor/CapturadorPosicion.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class CapturadorPosicion : EditorWindow
+{
+
+    Vector3 PosicionObjeto;
+
+    Vector3 PosicionPanelHijo;
+
+    Vector3 Posicion2PanelHijo;
+
+
+    GameObject ObjetoaCapturar;
+
+
+    [MenuItem("Invenza Creator SDK/Capturar Posición")]
+    /**
+     * Name: CrearVentana
+     * Description: Crea la ventana para capturar la posicion de el GameObject
+     * Params: NO
+     * Return: NO
+     * 
+     * */
+    public static void CrearVentana()
+    {
+        GetWindow(typeof(CapturadorPosicion));
+    }
+
+    /**
+  * Name: OnGUI
+  * Description: Crea la ventana junto con las opciones internas
+  * Params: NO
+  * Return: la ventana dentro del editor con las opciones que se le atribuyen, en este caso un vector3 para la posicion y un objeto a cargar
+  * 
+  * */
+    private void OnGUI()
+    {
+        GUILayout.Label("Posicion del Objeto", EditorStyles.boldLabel);
+
+
+        PosicionObjeto = EditorGUILayout.Vector3Field("Posicion del Objeto", PosicionObjeto);
+
+
+        PosicionPanelHijo = EditorGUILayout.Vector3Field("Posicion del hijo", PosicionPanelHijo);
+
+        Posicion2PanelHijo = EditorGUILayout.Vector3Field("Posicion del segundo hijo", Posicion2PanelHijo);
+
+
+        ObjetoaCapturar = EditorGUILayout.ObjectField("Objeto a Capturar", ObjetoaCapturar, typeof(GameObject), true) as GameObject;
+
+        if (GUILayout.Button("Capturar la Posición"))
+        {
+            CapturarPosicion();
+        }
+
+
+    }
+
+
+
+    /**
+* Name: CapturarPosicion
+* Description: Captura la posicion del GameObject puesto en la variable
+* Params: NO
+* Return: se ve la posicion dentro de la ventana de editor creada
+* 
+* */
+    private void CapturarPosicion()
+    {
+        if (ObjetoaCapturar==null)
+        {
+            Debug.LogError("No Existe un objeto en el campo Objeto a Capturar");
+            return;
+        }
+
+        PosicionObjeto = ObjetoaCapturar.transform.position;
+
+        PosicionPanelHijo = ObjetoaCapturar.transform.GetChild(0).transform.position;
+
+        Posicion2PanelHijo = ObjetoaCapturar.transform.GetChild(1).transform.position;
+
+    }
+
+
+
+
+
+}
